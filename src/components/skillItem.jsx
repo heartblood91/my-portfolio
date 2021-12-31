598afe5b71860833ad39db29e42ade302f0a762f
heartblood91 <github@hidemyhome.ovh>
2021-12-31 18:36:00 +0100

    [update] value of each skills + change wording of each tooltip

--- src/components/skillItem.jsx
+++ src/components/skillItem.jsx
@@ -1,42 +1,86 @@
 import React from "react";
 import ReactTooltip from "react-tooltip";
 
-const SkillItem = (props) => {
-  // Réalise un tooltip si besoin
-  const renderTooltip = () => {
-    if (props.tooltip !== null) {
-      return <ReactTooltip id={props.forWhat}>{props.tooltip}</ReactTooltip>;
-    }
-  };
-
-  // Réalise le rendu de l'ensemble des titres
-  const titleOfProgressBar = () => {
-    return props.title + " " + props.value + "%";
-  };
-
+const SkillItem = ({
+  value,
+  title,
+  tooltip,
+}) => {
   return (
     <div className="progress shadow-box-light">
       <div
         className="progress-bar"
         role="progressbar"
-        aria-valuenow={props.value}
+        aria-valuenow={value}
         aria-valuemin="0"
         aria-valuemax="100"
         data-tip="tooltip"
         data-place="top"
-        data-for={props.forWhat}
+        data-for={tooltip.id}
         data-background-color="#002f68da"
-        style={{ width: props.value + "%" }}
+        style={{ width: value + "%" }}
       >
-        {props.title.length > 20 ? (
-          <h6> {titleOfProgressBar()} </h6>
+        {title.length > 20 ? (
+          <h6> {title} </h6>
         ) : (
-          <h5>{titleOfProgressBar()}</h5>
+          <h5>{title}</h5>
         )}
-        {renderTooltip()}
+        <Tooltip {...tooltip} />
       </div>
     </div>
   );
 };
+
+const Tooltip = ({ 
+  is_enabled,
+  number_of_personnal_project,
+  number_of_pro_project,
+  id,
+}) => {
+  const tooltip = React.useMemo(() => {
+    const personnal_project_wording_in_plural = putInPlural({
+      number: number_of_personnal_project,
+      word: 'projet'
+    })
+    const pro_project_wording_in_plural = putInPlural({
+      number: number_of_pro_project,
+      word: 'projet'
+    })
+    const text_for_personnal_project = `${number_of_personnal_project} ${personnal_project_wording_in_plural} perso`
+    const text_for_pro_project = `${number_of_pro_project} ${pro_project_wording_in_plural} pro`
+
+    if (number_of_pro_project && number_of_personnal_project) {
+      return `${text_for_personnal_project} & ${text_for_pro_project}`
+    } else if (number_of_pro_project) {
+      return text_for_pro_project
+    } else if (number_of_personnal_project) {
+      return text_for_personnal_project
+    } else {
+      return ""
+    }
+  }, [
+    number_of_personnal_project,
+    number_of_pro_project,
+  ])
+
+  if (is_enabled) {
+    return (
+      <ReactTooltip
+        id={id}>
+        {tooltip}
+      </ReactTooltip>
+    )
+  } else {
+    return <React.Fragment />
+  }
+}
+
+const putInPlural = ({number, word}) => {
+  if (2 <= number) {
+    return `${word}s`
+  } else {
+    return word
+  }
+}
 
 export default SkillItem;
