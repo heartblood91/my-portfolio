1bd14fb5a44fd35c19f494396350e2c014361bc7
heartblood91 <github@hidemyhome.ovh>
2022-01-01 18:58:34 +0100

    [update] add carousel on each secret project (like pro or private)

--- src/portfolio.jsx
+++ src/portfolio.jsx
@@ -8,8 +8,21 @@ import pictureOfWebWorldBucketList from "./content/images/WorldBucketList.webp";
 import pictureOfGeoConfinement from "./content/images/GeoConfinement.webp";
 import pictureOfCoraDriveAssistant from "./content/images/CoraDriveAssistant.webp";
 import pictureOfGithub from "./content/images/github.webp";
+import Carousel from "./components/carousel";
 
 const Portfolio = (props) => {
+  const array_of_items_for_examin_carousel = [
+    { img_src: pictureOfWebChat, img_alt: "1" },
+    { img_src: pictureOfWebMarkdown, img_alt: "2" },
+  ]
+  const array_of_items_for_mi_carousel = [
+    { img_src: pictureOfWebChat, img_alt: "1" },
+    { img_src: pictureOfWebMarkdown, img_alt: "2" },
+  ]
+  const array_of_items_for_account_carousel = [
+    { img_src: pictureOfWebChat, img_alt: "1" },
+    { img_src: pictureOfWebMarkdown, img_alt: "2" },
+  ]
   return (
     <section id="portfolio">
       <div className="container">
@@ -18,8 +31,43 @@ const Portfolio = (props) => {
           <h2>Portfolio</h2>
         </div>
         <h3 className="my-4 shadow-black">
+          <u>Projets Professionnels</u>
+        </h3>
+        <p className="my-4 text-black font-italic text-center">
+          Pour des raisons de droit d'auteur, vous pourrez appercevoir seulement des captures d'écran. <br/>
+          Certaines informations seront floutées.
+        </p>
+        <h4 className="my-4 text-grey shadow-black">Examin</h4>
+
+        <div className="col-md-8 col-sm-12 mx-auto d-block">
+          <Carousel
+            array_of_items={array_of_items_for_examin_carousel}
+            id='examin_carousel'
+          />
+        </div>
+
+        <h4 className="my-4 text-grey shadow-black">Ministère de l'intérieur</h4>
+        <div className="col-md-8 col-sm-12 mx-auto d-block">
+          <Carousel
+            array_of_items={array_of_items_for_mi_carousel}
+            id='mi_carousel'
+          />
+        </div>
+
+        <h3 className="my-4 shadow-black">
           <u>Projets Personnels</u>
         </h3>
+        <h4 className="my-4 text-grey shadow-black">Money Management (Application web)</h4>
+        <p className="my-4 text-black font-italic text-center">
+          Pour des raisons de confidentialités, vous pourrez appercevoir seulement des captures d'écran. <br/>
+          Certaines informations seront floutées.
+        </p>
+        <div className="col-md-8 col-sm-12 mx-auto d-block">
+          <Carousel
+            array_of_items={array_of_items_for_account_carousel}
+            id='account_carousel'
+          />
+        </div>
         <h4 className="my-4 text-grey shadow-black">Application Android</h4>
         <div className="row">
           <PortFolioThumbs
