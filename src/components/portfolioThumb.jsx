8d3dc04871b4ce648797e75be4df3bb5030c01bc
heartblood91 <github@hidemyhome.ovh>
2022-01-01 18:59:04 +0100

    [patch] bad behavior for link img

--- src/components/portfolioThumb.jsx
+++ src/components/portfolioThumb.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PortFolioThumbs = (props) => {
   return (
-    <div className="col-lg-3 col-md-4 col-sm-6">
+    <div className="col-lg-3 col-md-4 col-sm-6 img-url">
       <a href={props.link} target="_blank" rel="noopener noreferrer">
         <img
           className="img-thumbnail"
